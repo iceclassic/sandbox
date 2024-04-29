ca32d09ebde29e975c72c2c5b436cb2c176e6a54
Gabriel Follet <g.f.follet@student.tudelft.nl>
2024-04-29 11:18:29 -0400

    reordering-folders

---
+++ Notebooks/numpyfy.py
@@ -0,0 +1,37 @@
+def Filter_Numpify(df,T_0,T_f,numpyfy=False,multiyear=[]):
+    """ 
+
+    numpyfy:
+    ================================
+    Converts the Df to numpy:
+        Df.index gets transformed to three columns [Year][Month][Day]
+        Column names get deleted as the array only contains .type(float64) 
+
+    filter
+    ================================
+    T_0= initial date for mask, format = ` 'yyyy-mm-dd' `
+    T_f= initial date for mask, format = ` 'yyyy-mm-dd'
+     
+    If multiyear argument is present we filter the selected years ( only accepts list of years) and we ignore T0 anf Tf
+
+    
+    """
+    if multiyear:
+        print('multi')
+        df = df.loc[df.index.year.isin(multiyear)]
+    else:
+        mask = df.index.to_series().between(T_0,T_f)
+        df=df[mask]
+
+    if numpyfy:
+        df['Year'] = df.index.year.astype(int)
+        df['Month'] =df.index.month.astype(int)
+        df['Day'] = df.index.day.astype(int)
+        # cols = df.columns.tolist()
+        # cols = ['Year', 'Month', 'Day'] + cols[:-3]
+        # df = df[cols]
+
+       # df = df.reset_index(drop=True)
+        df = df.values
+        print("Dataframe has been -numpified- !!!\n The original datetime index has been converted to three columns,containing year,month and day,in position -3,-2,-1.")
+    return df    
