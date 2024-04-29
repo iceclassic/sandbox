13deafe641d49a85291e36477bfad47f2c670361
Gabriel Follet <g.f.follet@student.tudelft.nl>
2024-04-29 12:29:35 -0400

    deleted useless stuff

--- Notebooks/numpyfy.py
+++ Notebooks/numpyfy.py
@@ -27,11 +27,11 @@ def Filter_Numpify(df,T_0,T_f,numpyfy=False,multiyear=[]):
         df['Year'] = df.index.year.astype(int)
         df['Month'] =df.index.month.astype(int)
         df['Day'] = df.index.day.astype(int)
-        # cols = df.columns.tolist()
-        # cols = ['Year', 'Month', 'Day'] + cols[:-3]
-        # df = df[cols]
+        cols = df.columns.tolist()
+        cols = ['Year', 'Month', 'Day'] + cols[:-3]
+        df = df[cols]
 
-       # df = df.reset_index(drop=True)
+        df = df.reset_index(drop=True)
         df = df.values
         print("Dataframe has been -numpified- !!!\n The original datetime index has been converted to three columns,containing year,month and day,in position -3,-2,-1.")
     return df    
